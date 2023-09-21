2cd8c20246efbcf2ddac814d530eb87cd8f8491d
alifeee <alifeeerenn@hotmail.com>
2023-09-21 13:01:43 +0100

    release v0.2.0

--- mumble/plugin.c
+++ mumble/plugin.c
@@ -73,7 +73,7 @@ void mumble_releaseResource(const void *pointer)
 
 mumble_version_t mumble_getVersion()
 {
-	char *VERSION = "0.1.0";
+	char *VERSION = "0.2.0";
 
 	mumble_version_t version;
 	char *v = malloc(strlen(VERSION) + 1);
